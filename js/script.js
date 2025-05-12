c626e3979733bef3eb8bc8bb265d69581c0c0a35
santimgb98 <santimgb98@gmail.com>
2025-05-12 13:05:00 +0200

    ya se añaden actividades nuevas, pero hay fallo en que el primer texto siempre se repite con el último y solo funcionan los botones de la primera actividad

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(()=>{
 
     // TAREA
-    $(".tarea").mouseenter(()=>{
-        $(".tarea").css("transform","scale:1.2")
+    $("#tarea").mouseenter(()=>{
+        $("#tarea").css("transform","scale:1.2")
     });
 
 
     // BOTÓN DE CHECK
     $("#btn_completada").click(()=>{
-        $(".tarea").css("background","rgb(126, 255, 79)");
+        $("#tarea").css("background","rgb(126, 255, 79)");
     });
     $("#btn_completada").mouseenter(()=>{
         $("#btn_completada").css("background","rgb(144, 239, 80)");
@@ -21,7 +21,7 @@ $(document).ready(()=>{
 
     // BOTÓN DE ELIMINAR
     $("#btn_eliminar").click(()=>{
-        $(".tarea").slideUp(150);
+        $("#tarea").slideUp(150);
     });
     $("#btn_eliminar").mouseenter(()=>{
         $("#btn_eliminar").css("background","rgb(254, 153, 153)");
@@ -32,28 +32,24 @@ $(document).ready(()=>{
 
     // BOTÓN DE DESMARCAR
     $("#btn_desmarcar").click(()=>{
-        $(".tarea").css("background","");
+        $("#tarea").css("background","");
     })
     
 
     // AÑADIR TAREA A LA LISTA
-    const crear_tarea = ()=>{
-        /*const tarea = $("<div class='tarea'>");
-        $("#contenido").append(tarea);
 
-        const texto = ("<p id='input_tarea'></p>").val("<input id='input_add'>");
-        const input_completada = $("<button id='btn_completada' class='btn></button>").text('✔️');
-        const input_eliminar = $("<button id='btn_eliminar' class='btn'></button>").text('❌');
-        const input_desmarcar = $("<button id='btn_desmarcar' class='btn'></button>").text('➖');
-        $(".tarea").append(texto,input_completada,input_eliminar,input_desmarcar)*/
-
-        $(".tarea").clone().appendTo("#contenido");
-        
-    }
-   
+    $("#tarea").hide()
     $("#btn_add").click(()=>{
-        crear_tarea();
-        
+        crearTarea();
+        $("#tarea").show()
     });
+
+    const crearTarea = ()=>{
+        const nueva_tarea = $("#input_add").val();
+        $("#input_tarea").text(nueva_tarea);
+        const tarea = ("#tarea")
+        $(tarea).clone(true).appendTo("#contenido");
+
+    }
 
 });
