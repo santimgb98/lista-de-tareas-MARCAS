3ec3e70d3856f579b0f16be079447cd05ee7697d
santimgb98 <santimgb98@email.com>
2025-05-13 19:59:06 +0200

    completada

--- js/script.js
+++ js/script.js
@@ -1,55 +1,54 @@
-$(document).ready(()=>{
-
-    // TAREA
-    $("#tarea").mouseenter(()=>{
-        $("#tarea").css("transform","scale:1.2")
-    });
-
-
-    // BOTÓN DE CHECK
-    $("#btn_completada").click(()=>{
-        $("#tarea").css("background","rgb(126, 255, 79)");
-    });
-    $("#btn_completada").mouseenter(()=>{
-        $("#btn_completada").css("background","rgb(144, 239, 80)");
+$(document).ready(() => {
     
-    });
-    $("#btn_completada").mouseleave(()=>{
-        $("#btn_completada").css("background","rgb(227, 255, 221)");
-    });
-
-
-    // BOTÓN DE ELIMINAR
-    $("#btn_eliminar").click(()=>{
-        $("#tarea").slideUp(150);
-    });
-    $("#btn_eliminar").mouseenter(()=>{
-        $("#btn_eliminar").css("background","rgb(254, 153, 153)");
-    });
-    $("#btn_eliminar").mouseleave(()=>{
-        $("#btn_eliminar").css("background","rgb(255, 221, 221)");
-    });
-
-    // BOTÓN DE DESMARCAR
-    $("#btn_desmarcar").click(()=>{
-        $("#tarea").css("background","");
-    })
-    
+    let contador = 0;
+    const max_tareas = 7;
 
     // AÑADIR TAREA A LA LISTA
-
-    $("#tarea").hide()
-    $("#btn_add").click(()=>{
-        crearTarea();
-        $("#tarea").show()
+    $("#btn_add").click(() => {
+        if(contador < max_tareas){
+            crearTarea();
+            contador++;
+        } else { 
+            alert("No se pueden crear más tareas");
+        }
     });
 
-    const crearTarea = ()=>{
+    const crearTarea = () => {
         const nueva_tarea = $("#input_add").val();
-        $("#input_tarea").text(nueva_tarea);
-        const tarea = ("#tarea")
-        $(tarea).clone(true).appendTo("#contenido");
 
-    }
+        const tarea = `<div class="tarea">
+                <p class="input_tarea">${nueva_tarea}</p>
+                <button class="btn btn_completada">✔️</button>
+                <button class="btn btn_eliminar">✖️</button>
+                <button class="btn btn_desmarcar">➖</button>
+            </div>`;
 
+        $(tarea).prependTo("#contenido");
+        $("#input_add").val("");
+
+        const $nuevaTarea = $(".tarea").first();
+
+        // Botón COMPLETADA
+        $nuevaTarea.find(".btn_completada").click(function() {
+            $(this).closest(".tarea").css("background", "rgb(126, 255, 79)");
+            $(this).css("background", "rgb(144, 239, 80)");
+        });
+
+        // Botón ELIMINAR
+        $nuevaTarea.find(".btn_eliminar").click(function() {
+            const $tarea = $(this).closest(".tarea");
+            $tarea.slideUp(150, function() {
+                $tarea.remove();
+                $(this).css("background", "rgb(254, 153, 153)");
+                // Contador resta, porque al eliminar tarea le resta
+                // respecto al número máximo que puede haber
+                contador--;
+            });
+        });
+
+        // Botón DESMARCAR
+        $nuevaTarea.find(".btn_desmarcar").click(function() {
+            $(this).closest(".tarea").css("background", "");
+        });
+    };
 });
